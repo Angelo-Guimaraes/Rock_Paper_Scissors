61ae082a28a86d5f2dc4575244da1e5a843f9dbb
Angelo Sa <afrsg92@gmail.com>
2021-05-26 18:15:41 +0100

    final entry (console only)

--- script.js
+++ script.js
@@ -1,11 +1,63 @@
 //creat var
+var roundPlayer=0;
+var roundComp=0;
+var roundDraw=0;
+//inc according to resulte until 5points
+
+function round(){
+
+   while ((this.roundPlayer < 5) && (this.roundComp < 5)) {
+    
+    playRound()
+   }
+    if (this.roundPlayer == 5) {
+        return "You WIN";
+    }
+
+    else{
+        return "You lose"
+    }
+
+}
 
 //save user pick
 
 //pc chooses one of the three 
+function computerPlays(){
+    let options = ["rock","paper","scissors"]
+    return options[Math.floor(Math.random()*options.length)]
+}
 
 //compares both picks with all the options
+function playRound(){
+    let message = ""
+    let humanPlays = prompt("rock,paper,scissors?");
+    //give result 
+    if (humanPlays == computerPlays()){
+        this.roundDraw++;
+        message = "It's a tie"
+    }
 
-//give result 
+    if  ((humanPlays=="rock" && computerPlays()=="scissors")||
+        (humanPlays=="paper" && computerPlays()=="rock")||
+        (humanPlays=="scissors" && computerPlays()== "paper")) {
+        this.roundPlayer++;
 
-//inc according to resulte until 5points
+        message = "You win!"
+    }
+
+    else {
+        this.roundComp++;   
+
+        message = "You lose!"
+    }
+
+    return message
+
+}
+
+
+
+
+
+
